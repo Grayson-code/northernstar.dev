e075af7b3ad284724b726332998deaff1da1a382
Grayson-code <northernstarmusic10@gmail.com>
2022-08-11 21:53:32 +0530

    a

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,8 @@ const Home: NextPage = () => {
           <title>About</title>
           <meta property='og:title' content="Northern Stars Profile" />
           <meta property='og:url' content="https://northernstar.vercel.app/" />
-          <meta property='og:type' content="article" />
-          <meta property='description' content='Hello, I&apos;m a random dev, music producer living in Kerala. I prefer NextJS for my projects!' />
+          <meta property='og:type' content="article" /> 
+          <meta name='description' content='Hello, I&apos;m a random dev, music producer living in Kerala. I prefer NextJS for my projects!' />
           <meta property='og:image' content='/public/Porter Robinson & Kero Kero Bonito - _Unreleased 2019 Demo_ (Fan-Made Cover) 0-3 screenshot.png'/>
           <meta property='og:description' content="Hello, I&apos;m a random dev, music producer living in Kerala. I prefer NextJS for my projects!" />
           <meta property='twitter:card' content='summary' />
