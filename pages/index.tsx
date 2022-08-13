0c6654151499db019fd25d55c64d058778e701ee
Grayson-code <northernstarmusic10@gmail.com>
2022-08-13 11:25:35 +0530

    aaa

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,29 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
+import type { NextPage } from "next"
+import Head from "next/head"
+import Link from "next/link"
 
 const Home: NextPage = () => {
   return (
     <section>
-      <div className='flex text-white'>
+      <div className="flex text-white">
         <Head>
           <html lang="en" />
           <title>About</title>
-          <meta property='og:title' content="Northern Stars Profile" />
-          <meta property='og:url' content="https://northernstar.vercel.app/" />
-          <meta property='og:type' content="article" />
-          <meta property='og:image' content='/public/Porter Robinson & Kero Kero Bonito - _Unreleased 2019 Demo_ (Fan-Made Cover) 0-3 screenshot.png' />
-          <meta property='og:description' content="Hello, I&apos;m a random dev, music producer living in Kerala. I prefer NextJS for my projects!" />
-          <meta name='description' content='Hello, I&apos;m a random dev, music producer living in Kerala. I prefer NextJS for my projects!' />
-          <meta name='twitter:card' content='summary' />
-          <meta name='twitter:site' content='@NorthernStare' />
-          <meta name='twitter:title' content='About' />
-          <meta name='twitter:description' content='Northern Stars Personal Website' />
-          <meta name='twitter:image' content='/public/Porter Robinson & Kero Kero Bonito - _Unreleased 2019 Demo_ (Fan-Made Cover) 0-3 screenshot.png' />
-          <meta name='twitter:url' content='https://northernstar.vercel.app' />
+          <meta property="og:title" content="Northern Stars Profile" />
+          <meta property="og:url" content="https://northernstar.vercel.app/" />
+          <meta property="og:type" content="article" />
+          <meta property="og:image" content="/public/Porter Robinson & Kero Kero Bonito - _Unreleased 2019 Demo_ (Fan-Made Cover) 0-3 screenshot.png" />
+          <meta property="og:description" content="Hello, I'm a random dev, music producer living in Kerala. I prefer NextJS for my projects!" />
+          <meta name="description" content="Hello, I'm a random dev, music producer living in Kerala. I prefer NextJS for my projects!" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@NorthernStare" />
+          <meta name="twitter:title" content="About" />
+          <meta name="twitter:description" content="Northern Stars Personal Website" />
+          <meta name="twitter:image" content="/public/Porter Robinson & Kero Kero Bonito - _Unreleased 2019 Demo_ (Fan-Made Cover) 0-3 screenshot.png" />
+          <meta name="twitter:url" content="https://northernstar.vercel.app" />
         </Head>
         <article>
-          <div className='absolute bottom-0 left-0'>
+          <div className="absolute bottom-0 left-0">
             <div
               className="p-4 border rounded text-amber-700 bg-amber-50 border-amber-900/10"
               role="alert"
@@ -35,16 +35,16 @@ const Home: NextPage = () => {
               </p>
             </div>
           </div>
-          <h1 className='flex justify-center text-4xl text-center w-screen bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple to-pink-900'>
+          <h1 className="flex justify-center text-4xl text-center w-screen bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple to-pink-900">
             About
           </h1>
-          <h1 className='flex justify-center'>
-            <a className='p-2 bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple to-pink-900'>About</a>
-            <Link href={'/tech'} className="">
-              <a className='p-2 underline font-bold'>Tech</a>
+          <h1 className="flex justify-center">
+            <a className="p-2 bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple to-pink-900">About</a>
+            <Link href={"/tech"} className="">
+              <a className="p-2 underline font-bold">Tech</a>
             </Link>
           </h1>
-          <p className='flex flex-col justify-center items-center h-screen'>
+          <p className="flex flex-col justify-center items-center h-screen">
             Hello, I&apos;m a random dev, music producer living in Kerala. I prefer NextJS for my projects!
 
           </p>
