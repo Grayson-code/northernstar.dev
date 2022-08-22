9ce8237829eeb65676b35a0b30670ac4c03f3ea9
Grayson-code <northernstarmusic10@gmail.com>
2022-08-22 16:12:05 +0530

    temp fix to meta name issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,14 +14,14 @@ const Home: NextPage = () => {
           <meta property="og:type" content="article" />
           <meta property="og:image" content="/public/Porter Robinson & Kero Kero Bonito - _Unreleased 2019 Demo_ (Fan-Made Cover) 0-3 screenshot.png" />
           <meta property="og:description" content="Hello, I'm a random dev, music producer living in Kerala. I prefer NextJS for my projects!" />
-          <meta name="description" content="Hello, I'm a random dev, music producer living in Kerala. I prefer NextJS for my projects!"></meta>
+          {/* <meta name="description" content="Hello, I'm a random dev, music producer living in Kerala. I prefer NextJS for my projects!"></meta>
           <meta name="twitter:card" content="summary"></meta>
           <meta name="twitter:site" content="@NorthernStare" ></meta>
           <meta name="twitter:title" content="About" ></meta>
           <meta name="twitter:description" content="Northern Stars Personal Website"></meta>
           <meta name="twitter:image" content="/public/Porter Robinson & Kero Kero Bonito - _Unreleased 2019 Demo_ (Fan-Made Cover) 0-3 screenshot.png" ></meta>
           <meta name="twitter:image:alt" content="a visualizer"></meta>
-          <meta name="twitter:url" content="https://northernstar.vercel.app" ></meta>
+          <meta name="twitter:url" content="https://northernstar.vercel.app" ></meta> */}
         </Head>
         <article>
           <div className="absolute bottom-0 left-0">
